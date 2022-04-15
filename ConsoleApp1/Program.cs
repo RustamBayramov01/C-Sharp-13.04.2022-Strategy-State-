64307b78bde0da6e40eed0dda5db955faecb79e9
RustamBayramov01 <rustam.bayramov.989@gmail.com>
2022-04-15 09:44:02 +0400

    C Sharp 13.04.2022 [ Strategy , State ]

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public interface ICompression { void CompressFolder(string compressedArchiveFileName); }
+
+    public class RarCompression : ICompression
+    {
+        public void CompressFolder(string compressedArchiveFileName)
+        {
+            Console.WriteLine("Qovluq Rar yanaşmasından istifadə edərək sıxılır : '" + compressedArchiveFileName + ".rar' fayl yaradılmışdır");
+        }
+    }
+
+    public class ZipCompression : ICompression
+    {
+        public void CompressFolder(string compressedArchiveFileName)
+        {
+            Console.WriteLine("Qovluq Zip yanaşmasından istifadə edərək sıxılır: '" + compressedArchiveFileName + ".zip' fayl yaradılmışdır");
+        }
+    }
+
+    public class CompressionContext
+    {
+        private ICompression Compression;
+
+        public CompressionContext(ICompression Compression) { this.Compression = Compression; }
+        public void SetStrategy(ICompression Compression) { this.Compression = Compression; }
+        public void CreateArchive(string compressedArchiveFileName) { Compression.CompressFolder(compressedArchiveFileName); }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CompressionContext ctx = new CompressionContext(new ZipCompression());
+            ctx.CreateArchive("DotNetDesignPattern");
+            ctx.SetStrategy(new RarCompression());
+            ctx.CreateArchive("DotNetDesignPattern");
+            Console.Read();
+        }
+    }
+}
